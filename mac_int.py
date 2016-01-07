c613df57cff1d29ade7ad4b4e2cb771c2ad4a582
Peter Juhl Savnik <s113556@student.dtu.dk>
2016-01-07 14:16:22 +0000

    Display 2 works with intention in radiation

--- mac_int.py
+++ mac_int.py
@@ -12,6 +12,8 @@ import time
 import datetime
 import os.path
 from sense_hat import SenseHat
+import hashlib
+import os
 
 class Mac_int:
 
@@ -29,13 +31,14 @@ class Mac_int:
 		#Shared Variables
 		self.mac_list = [[]] 	# [time, mac addr, signal level]
 		self.white_list = []	# whitelist of mac addr
-		self.dt = 1 		# sample time in minutes
+		self.dt = 10 		# sample time in minutes
 
 		# Init wifi
 		try:
 			Wireless(interface).setMode('Monitor')
 			Wireless(interface).setFrequency(freq)
 		except ValueError:
+			os.system("sh wifi_setup.sh")
 			return "Wifi Initialisation failed!"
 		
 		self.loadWhiteList()		
@@ -82,7 +85,7 @@ class Mac_int:
 
 	# Add row with observation to log
 	def log_data_append(self, time, mac, siglevel):
-		row = [[time,mac,siglevel]]
+		row = [[time,self.whitelist_filter(mac),siglevel]]
 		with open('mac_log.txt', 'a') as f:
 			writer = csv.writer(f)
 			writer.writerows(row)
@@ -113,42 +116,101 @@ class Mac_int:
 				return "Creating Whitelist file failed"
 
 			print "Whitelist file created"
-		
+	
+	# hash all non white list mac addr
+	def whitelist_filter(self, mac):
+		print 
+		if self.white_list.count(mac) > 0:
+			return mac
+		else:
+			hash_object = hashlib.sha256(b'mac') 	# creates a hash object
+			hash_hex = hash_object.hexdigest()	# takes the hash and outputs it in hex
+			return hash_hex
+	
 
 	# perodic updating dt
 	def perodic(self):
 		# remove duplets in dt time interval
 		mac_list_dt = []
-		print self.mac_list
+		#print self.mac_list
 		for mac in self.mac_list:
 			if mac: 	# Remove empty entities
 				
 				# if one or more elements in list check for duplet
 				is_duplet = sum(x.count(mac[1]) for x in mac_list_dt)
 				# check time stamp
-				print mac
+				#print mac
 				time_mac = datetime.datetime.strptime(mac[0],"%Y-%m-%d %H:%M:%S")
 				time_now = datetime.datetime.now()
 				delta_t = time_now + datetime.timedelta(minutes = -self.dt)
-				print delta_t
-				if time_mac >= delta_t: print "OK"
+				#print delta_t
+				#if time_mac >= delta_t: print "OK"
 
 				if is_duplet < 1 and time_mac >= delta_t:
 					mac_list_dt.append(mac)
-					print "Added!"	
+					#print "Added!"	
 		
 		count_unique_macs = len(mac_list_dt)
 		self.unique_macs_dt = count_unique_macs
 		print "Count of Unique MACs: %d" % (count_unique_macs)
-		self.display()
-			
+		
+		self.mac_list_dt = mac_list_dt
+		self.display2()
+		
 
 
-	# visualise
+	# visualise with scrolling text
 	def display(self):
 		sense = SenseHat()
 		sense.show_message(str(self.unique_macs_dt), text_colour=[255,0,2])
 
+	
+	# virtualise graphical
+	def display2(self):
+		# Levels of dist
+		d1 = self.int2rgb(self.siglevel_dt(-20,0))
+		d2 = self.int2rgb(self.siglevel_dt(-40,-21))
+		d3 = self.int2rgb(self.siglevel_dt(-60,-41))
+		d4 = self.int2rgb(self.siglevel_dt(-100,-61))
 		
+		#print self.siglevel_dt(-50,0)	
+
+		sense = SenseHat()
+		pixels = [
+		d4,d4,d4,d4,d4,d4,d4,d4,
+		d4,d3,d3,d3,d3,d3,d3,d4,
+		d4,d3,d2,d2,d2,d2,d3,d4,
+		d4,d3,d2,d1,d1,d2,d3,d4,
+		d4,d3,d2,d1,d1,d2,d3,d4,
+		d4,d3,d2,d2,d2,d2,d3,d4,
+		d4,d3,d3,d3,d3,d3,d3,d4,
+		d4,d4,d4,d4,d4,d4,d4,d4]
+		
+		sense.set_pixels(pixels)
+
+	 
+	def int2rgb(self,value):
+		if value < 1:	# White
+			return [0,0,0]
+		elif value < 5:	# blue
+			return [0,0,255]
+		elif value < 10: # green
+			return [0,255,0]
+		elif value < 15: #yellow
+			return [255,255,0]
+		else:		# red
+			return [255,0,0]
+
+	# Count # of mac addr in signal level interval
+	def siglevel_dt(self,low,high):
+		count = 0
+		for mac in self.mac_list_dt:
+			# if within the interval count up
+			if mac[2] >= low and mac[2] <= high:
+				count = count+1
+		return count
+
+
+
 mac_int = Mac_int()
 
