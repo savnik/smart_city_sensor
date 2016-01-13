6deba69fd8b636aee9bbc0aa865be2d2b46e094d
Peter Savnik <systems@savnik.dk>
2016-01-13 12:00:14 +0100

    Matlab fix missing data

--- 2016-01-12 data from 101/data_processing.m
+++ 2016-01-12 data from 101/data_processing.m
@@ -9,42 +9,51 @@ dt = 1
 
 display('Import data')
 % import data
-[time11,mac11,siglevel11] = import_log('1 - NE/2016-1-12-10-log_anonymous.txt');
-[time12,mac12,siglevel12] = import_log('1 - NE/2016-1-12-11-log_anonymous.txt');
-[time13,mac13,siglevel13] = import_log('1 - NE/2016-1-12-13-log_anonymous.txt');
+[time11,mac11,siglevel11] = import_log('1 - NE/2016-1-12-8-log_anonymous.txt');
+[time12,mac12,siglevel12] = import_log('1 - NE/2016-1-12-9-log_anonymous.txt');
+[time13,mac13,siglevel13] = import_log('1 - NE/2016-1-12-10-log_anonymous.txt');
+[time14,mac14,siglevel14] = import_log('1 - NE/2016-1-12-11-log_anonymous.txt');
+[time15,mac15,siglevel15] = import_log('1 - NE/2016-1-12-13-log_anonymous.txt');
 
-[time21,mac21,siglevel21] = import_log('2 - SE/2016-1-12-10-log_anonymous.txt');
-[time22,mac22,siglevel22] = import_log('2 - SE/2016-1-12-11-log_anonymous.txt');
-[time23,mac23,siglevel23] = import_log('2 - SE/2016-1-12-13-log_anonymous.txt');
+[time21,mac21,siglevel21] = import_log('2 - SE/2016-1-12-8-log_anonymous.txt');
+[time22,mac22,siglevel22] = import_log('2 - SE/2016-1-12-9-log_anonymous.txt');
+[time23,mac23,siglevel23] = import_log('2 - SE/2016-1-12-10-log_anonymous.txt');
+[time24,mac24,siglevel24] = import_log('2 - SE/2016-1-12-11-log_anonymous.txt');
+[time25,mac25,siglevel25] = import_log('2 - SE/2016-1-12-13-log_anonymous.txt');
 
-[time31,mac31,siglevel31] = import_log('3 - SW/2016-1-12-10-log_anonymous.txt');
-[time32,mac32,siglevel32] = import_log('3 - SW/2016-1-12-11-log_anonymous.txt');
-[time33,mac33,siglevel33] = import_log('3 - SW/2016-1-12-12-log_anonymous.txt');
-[time34,mac34,siglevel34] = import_log('3 - SW/2016-1-12-13-log_anonymous.txt');
 
-[time41,mac41,siglevel41] = import_log('4 - NW/2016-1-12-10-log_anonymous.txt');
-[time42,mac42,siglevel42] = import_log('4 - NW/2016-1-12-11-log_anonymous.txt');
-[time43,mac43,siglevel43] = import_log('4 - NW/2016-1-12-13-log_anonymous.txt');
+[time31,mac31,siglevel31] = import_log('3 - SW/2016-1-12-8-log_anonymous.txt');
+[time32,mac32,siglevel32] = import_log('3 - SW/2016-1-12-9-log_anonymous.txt');
+[time33,mac33,siglevel33] = import_log('3 - SW/2016-1-12-10-log_anonymous.txt');
+[time34,mac34,siglevel34] = import_log('3 - SW/2016-1-12-11-log_anonymous.txt');
+[time35,mac35,siglevel35] = import_log('3 - SW/2016-1-12-12-log_anonymous.txt');
+[time36,mac36,siglevel36] = import_log('3 - SW/2016-1-12-13-log_anonymous.txt');
 
-time1 = [time11; time12; time13;];
-mac1 = [mac11;mac12;mac13];
-siglevel1 = [siglevel11;siglevel12;siglevel13];
-clearvars time11 time12 time13 mac11 mac12 mac13 siglevel11 siglevel12 siglevel13
+[time41,mac41,siglevel41] = import_log('4 - NW/2016-1-12-8-log_anonymous.txt');
+[time42,mac42,siglevel42] = import_log('4 - NW/2016-1-12-9-log_anonymous.txt');
+[time43,mac43,siglevel43] = import_log('4 - NW/2016-1-12-10-log_anonymous.txt');
+[time44,mac44,siglevel44] = import_log('4 - NW/2016-1-12-11-log_anonymous.txt');
+[time45,mac45,siglevel45] = import_log('4 - NW/2016-1-12-13-log_anonymous.txt');
 
-time2 = [time21; time22; time23;];
-mac2 = [mac21;mac22;mac23];
-siglevel2 = [siglevel21;siglevel22;siglevel23];
-clearvars time21 time22 time23 mac21 mac22 mac23 siglevel21 siglevel22 siglevel23
+time1 = [time11; time12; time13;time14;time15];
+mac1 = [mac11;mac12;mac13;mac14;mac15];
+siglevel1 = [siglevel11;siglevel12;siglevel13;siglevel14;siglevel15];
+clearvars time11 time12 time13 time14 time15 mac11 mac12 mac13 mac14 mac15 siglevel11 siglevel12 siglevel13 siglevel14 siglevel15
 
-time3 = [time31; time32; time33; time34];
-mac3 = [mac31;mac32;mac33; mac34];
-siglevel3 = [siglevel31;siglevel32;siglevel33; siglevel34];
-clearvars time31 time32 time33 time34 mac31 mac32 mac33 mac34 siglevel31 siglevel32 siglevel33 siglevel34
+time2 = [time21; time22; time23;time24;time25];
+mac2 = [mac21;mac22;mac23;mac23;mac24;mac25];
+siglevel2 = [siglevel21;siglevel22;siglevel23;siglevel24;siglevel25];
+clearvars time21 time22 time23 time24 time25 mac21 mac22 mac23 mac24 mac25 siglevel21 siglevel22 siglevel23 siglevel24 siglevel25
 
-time4 = [time41; time42; time43;];
-mac4 = [mac41;mac42;mac43];
-siglevel4 = [siglevel41;siglevel42;siglevel43];
-clearvars time41 time42 time43 mac41 mac42 mac43 siglevel41 siglevel42 siglevel43
+time3 = [time31; time32; time33; time34;time35;time36];
+mac3 = [mac31;mac32;mac33; mac34;mac35;mac36];
+siglevel3 = [siglevel31;siglevel32;siglevel33; siglevel34;siglevel35;siglevel36];
+clearvars time31 time32 time33 time34 time35 time36 mac31 mac32 mac33 mac34 mac35 mac36 siglevel31 siglevel32 siglevel33 siglevel34 siglevel35 siglevel36
+
+time4 = [time41; time42; time43; time44;time45];
+mac4 = [mac41;mac42;mac43;mac44;mac45];
+siglevel4 = [siglevel41;siglevel42;siglevel43;siglevel44;siglevel45];
+clearvars time41 time42 time43 time44 time45 mac41 mac42 mac43 mac44 mac45 siglevel41 siglevel42 siglevel43 siglevel44 siglevel45
 
 display('Count of observations:')
 n_data = [length(mac1), length(mac2), length(mac3), length(mac4)]
