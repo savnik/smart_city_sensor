e69662d02894a41fa1ec35b364fb1d7c0b305357
Peter Savnik <systems@savnik.dk>
2016-01-18 09:24:26 +0100

    Monday morning

--- 2016-01-13 data from basement/samples_at_dist.m
+++ 2016-01-13 data from basement/samples_at_dist.m
@@ -2,9 +2,9 @@ function [ time_filtered,mac_filtered,siglevel_filtered ] = samples_at_dist(time
 %SAMPLES_AT_DIST Summary of this function goes here
 %   Detailed explanation goes here
     % get data in time interval
-    [time_dt,mac_dt,siglevel_dt] = timeinterval(time,mac,siglevel,time_start,time_end);
+    [ time_filtered,mac_filtered,siglevel_filtered] = timeinterval(time,mac,siglevel,time_start,time_end);
     % take only one device and find all samples
-    [ time_filtered,mac_filtered,siglevel_filtered ] = one_mac_filter(time_dt,mac_dt,siglevel_dt, macA );
+    %[ time_filtered,mac_filtered,siglevel_filtered ] = one_mac_filter( time_filtered,mac_filtered,siglevel_filtered, macA );
 
 end
 
